fcdfa02a1b1a49e735109c75a4ae3e7d8e1732e3
dbyoung720 <dbyoung@sina.com>
2020-03-23 07:32:33 +0800

    add note

--- vcx86.cmd
+++ vcx86.cmd
@@ -2,7 +2,7 @@
 Color A
 setlocal EnableDelayedExpansion
   
-:: 设置网络代理
+:: 设置网络代理；如果没有网络代理，可以注释掉这两行代码；
 set http_proxy=http://127.0.0.1:41081
 set https_proxy=http://127.0.0.1:41081
 
@@ -15,7 +15,7 @@ set "Platform2=x86"
 set "Configure=Release"
 set "SetupPath=%CurrentCD%VSSDK\%Platform2%"
   
-:: 设置系统搜索路径；工具、第三方库都放在搜索路径中
+:: 设置系统搜索路径；工具、第三方库都放在搜索路径中；也可以放在系统搜索路径中；但最好放在文件中，因为 WINDOWS 系统的系统搜索路径有字符串长度限制；
 @set "sFile=%CurrentCD%Script\px86.txt"
 @set "sPath="
 @for /f "tokens=*" %%I in (%sFile%) do (set "sPath=!sPath!;%%I")
