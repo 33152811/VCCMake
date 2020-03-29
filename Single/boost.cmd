9637e493668db59545c8cea8ef4376127e0beaa6
dbyoung720 <dbyoung@sina.com>
2020-03-29 22:26:40 +0800

    modify build boost.cmd

--- Single/boost.cmd
+++ Single/boost.cmd
@@ -4,12 +4,13 @@ set SourceCodePath=%1
 set InstallSDKPath=%2
 set BuildPlatform_=%3
 
-if %BuildPlatform_% == x86 (
-  set PlatformModel=32
-) else (
+if %BuildPlatform_% == x64 (
   set PlatformModel=64
+) else (
+  set PlatformModel=32
 )
 
 cd %SourceCodePath%
+git.exe clean -d  -fx -f
 call bootstrap.bat
 b2 install --prefix=%InstallSDKPath% --toolset=msvc-14.1 address-model=%PlatformModel% link=static runtime-link=static  threading=multi
