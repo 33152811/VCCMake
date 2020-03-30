0112e87e6248aac32d7746e1314203d2f8b9de84
dbyoung720 <dbyoung@sina.com>
2020-03-30 13:18:14 +0800

    modify ImageMagick-windows.cmd

--- Single/ImageMagick-windows.cmd
+++ Single/ImageMagick-windows.cmd
@@ -3,18 +3,33 @@
 set SourceCodePath=%1
 set InstallSDKPath=%2
 set BuildPlatform_=%3
+set BuildLanguageX=%4
+set BakupCurrentCD=%5
+set SourceProjName=%6
 
 cd %SourceCodePath%
 
-bash --login -i -c "%SourceCodePath%/CloneRepositories.sh https://github.com/ImageMagick full"
+set "SourceCodePathLine=%SourceCodePath:\=/%"
 
-MSBuild.exe %SourceCodePath%\VisualMagick\configure\configure.sln^
+:: 下载子模块
+bash --login -i -c "./CloneRepositories.sh https://github.com/ImageMagick full"
+
+:: 检查是否有 patch 补丁文件
+ if exist "%BakupCurrentCD%\Patch\%SourceProjName%.patch" (
+   copy /Y "%BakupCurrentCD%\Patch\%SourceProjName%.patch" "%BakupCurrentCD%\Source\%SourceProjName%\%SourceProjName%.patch"
+   cd "%BakupCurrentCD%\Source\%SourceProjName%"
+   git apply "%SourceProjName%.patch"
+   del "%BakupCurrentCD%\Source\%SourceProjName%\%SourceProjName%.patch"
+ )
+
+MSBuild.exe ".\VisualMagick\configure\configure.sln"^
  /nologo /consoleloggerparameters:Verbosity=minimal /maxcpucount /nodeReuse:true^
  /target:Build /property:Configuration=Release;Platform=%BuildPlatform_%^
  /flp1:LogFile=zxerror.log;errorsonly;Verbosity=diagnostic^
  /flp2:LogFile=zxwarns.log;warningsonly;Verbosity=diagnostic
 
-call ".\VisualMagick\configure\configure.exe"
+cd "VisualMagick\configure"
+call configure.exe
 
 MSBuild.exe %SourceCodePath%\VisualMagick\VisualStaticMT.sln^
  /nologo /consoleloggerparameters:Verbosity=minimal /maxcpucount /nodeReuse:true^
--- Single/qt5.cmd
+++ Single/qt5.cmd
@@ -3,7 +3,18 @@
 set SourceCodePath=%1
 set InstallSDKPath=%2
 set BuildPlatform_=%3
+set BuildLanguageX=%4
+set BakupCurrentCD=%5
+set SourceProjName=%6
 set QTInstallPath=%InstallSDKPath%\QT5\static
+
+:: 检查是否有 patch 补丁文件
+ if exist "%BakupCurrentCD%\Patch\%SourceProjName%.patch" (
+   copy /Y "%BakupCurrentCD%\Patch\%SourceProjName%.patch" "%BakupCurrentCD%\Source\%SourceProjName%\%SourceProjName%.patch"
+   cd "%BakupCurrentCD%\Source\%SourceProjName%"
+   git apply "%SourceProjName%.patch"
+   del "%BakupCurrentCD%\Source\%SourceProjName%\%SourceProjName%.patch"
+ )
 
 cd %SourceCodePath%
 configure -confirm-license -opensource -platform win32-msvc -mp -debug-and-release -static -prefix "%QTInstallPath%" -nomake examples  -nomake tests
